4f2ba47fd68196e3a5125c4bc9abe1a90c224ce4
Kem2523 <krystalemorris@gmail.com>
2022-10-13 21:44:02 -0500

    Part 4

--- controllers/places.js
+++ controllers/places.js
@@ -7,14 +7,15 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/loaded.JPG'
       }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/images/cbr.JPG'
       }]
+      
       
       res.render('places/index', { places })
 
