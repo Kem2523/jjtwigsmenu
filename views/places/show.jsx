9ffedbbf9c981cda1a4c4dcead6037664c3462df
Kem2523 <krystalemorris@gmail.com>
2022-10-14 01:10:14 -0500

    part 8

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,7 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
+            <img src={data.place.pic} alt={data.place.name}></img>
             <h2>Description</h2>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
   Edit
